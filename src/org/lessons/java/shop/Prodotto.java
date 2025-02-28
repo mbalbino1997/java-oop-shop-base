a276669616e1aafed9b8992c9faeaa351eeb215f
mbalbino1997 <rmassimo.balbino@gmail.com>
2025-02-28 12:35:25 +0100

    add: incapsulamento

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -3,11 +3,11 @@ package org.lessons.java.shop;
 import java.util.Random;
 
 public class Prodotto {
-    public int code;
-    public String name;
-    public String description;
-    public double price;
-    public int iva;
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private int iva;
 
     public Prodotto(String name,String description, double price,int iva) {
         Random random = new Random();
@@ -18,6 +18,13 @@ public class Prodotto {
         this.iva=iva;
 
     }
+    public Prodotto(String name,double price) {
+        Random random = new Random();
+        this.code=random.nextInt(100);
+        this.name=name;
+        this.price=price;
+    }
+    
     public double basePrice() {
         return this.price;
     }
@@ -27,6 +34,43 @@ public class Prodotto {
     }
 public String fullName() {
     return this.code+"-"+this.name;
+}
+
+public int getCode() {
+    return code;
+}
+
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getDescription() {
+    return description;
+}
+
+public void setDescription(String description) {
+    this.description = description;
+}
+
+public double getPrice() {
+    return price;
+}
+
+public void setPrice(double price) {
+    this.price = price;
+}
+
+public int getIva() {
+    return iva;
+}
+
+public void setIva(int iva) {
+    this.iva = iva;
 }
 
     
